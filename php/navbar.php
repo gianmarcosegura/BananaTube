9bd8880f70a4a884ec83824794ea115df327cc87
viryinia9 <viryinia9@gmail.com>
2017-12-21 12:55:17 +0100

    iconos nuevos, navbar.css y navbar.php

---
+++ php/navbar.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+      <meta charset="utf-8"/>
+      <title>Bananatube</title>
+      <link rel="stylesheet" href="navbar.css" media="screen"/>
+      <link href="https://fonts.googleapis.com/css?family=Indie+Flower|Quicksand" rel="stylesheet">
+</head>
+   
+<body class="container">
+    <nav id="navbar">
+           
+            <!--He creado esta nueva categoría del menú, porque creo que debería acompañar al click en el logo, ya que no es tan intuitivo-->
+            <div id="inicio" class="over">
+                <div><img class="icon" src="imagenes/homepage.png"/></div>
+                <p><a href="#">Inicio</a></p>
+            </div>
+            
+            <br/>
+            <div id="categorias" class="over">
+                <ul>
+                    <li><a href="#">TEMÁTICA</a></li>
+                    <br/>
+                    <li><a href="#">FRANJAS DE EDAD</a></li>
+                    <br/>
+                    <li><a href="#">ÚLTIMOS VÍDEOS</a></li>
+                    <br/>
+                    <!--También he creado esta nueva categoría porque está en la página de inicio y creo que lo lógico es que esté también aquí-->
+                    <li><a href="">MÁS POPULARES</a></li>
+                </ul>
+            </div>
+            <br/>
+            
+            <div id="ajustes" class="over">
+                <div><img class="icon" src="imagenes/settings-work-tool.png"/></div>
+                <p><a href="#">Ajustes</a></p>
+            </div>
+    </nav>
+    
+</body>
+</html>
