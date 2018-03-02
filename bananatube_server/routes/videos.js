046d5f115862bcc4843f7446e4d56fc201dc5939
moli612 <lauramolinojimenez@gmail.com>
2018-03-02 16:43:05 +0100

    comienzo estructura

---
+++ bananatube_server/routes/videos.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function (req, res, next){
+    res.send('Listado de videos');
+});
+router.post('/', function (req, res, next){
+    res.send('Añadir un video');
+});
+router.get('/:id/', function (req, res, next){
+    res.send('Mostrar un video'+ req.params.id);
+});
+router.post('/:id/', function (req, res, next){
+    res.send('Editar un video' + req.params.id);
+});
+router.get('/delete/:id/', function (req, res, next){
+    res.send('Borrar un video' + req.params.id);
+});
+router.post('/upload/', function (req, res, next){
+    res.send('Subir un video');
+});
+
+module.exports = router;
