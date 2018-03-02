8713c0c8fb6aabadc16cb336112abe131699a529
elvira88 <elvira_alvarez1988@hotmail.com>
2018-03-02 16:47:00 +0100

    modificacion users

--- bananatube_server/routes/users.js
+++ bananatube_server/routes/users.js
@@ -3,7 +3,23 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('Listado de usuarios');
+});
+
+router.post('/', function(req, res, next) {
+  res.send('Añadir usuario');
+});
+
+router.get('/:id', function(req, res, next) {
+  res.send('Mostrar usuario '+ req.params.id);
+});
+
+router.post('/:id', function(req, res, next) {
+  res.send('Modificar usuario '+ req.params.id);
+});
+
+router.get('/delete/:id', function(req, res, next) {
+  res.send('Borrar usuario '+req.params.id);
 });
 
 module.exports = router;
