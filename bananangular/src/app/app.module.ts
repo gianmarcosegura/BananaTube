22ab2777e9f7fa84a3a7317aed11e3893ecb1dc6
moli612 <lauramolinojimenez@gmail.com>
2018-03-15 10:40:41 +0100

    mmm

--- bananangular/src/app/app.module.ts
+++ bananangular/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { EditProfileComponent } from './edit-profile/edit-profile.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { NavbaruserComponent } from './navbaruser/navbaruser.component';
 import { FooterComponent } from './footer/footer.component';
 <<<<<<< HEAD
 import { ComentarioComponent } from './comentario/comentario.component';
@@ -28,10 +29,15 @@ import { NavbaruserComponent } from './navbaruser/navbaruser.component';
     NavbarComponent,
     FooterComponent,
 <<<<<<< HEAD
+<<<<<<< HEAD
     ComentarioComponent
 =======
     NavbaruserComponent
 >>>>>>> 6ce320de83d3426ffa2a06976809841cc6ba9141
+=======
+    ComentarioComponent,
+    NavbaruserComponent
+>>>>>>> 73b98d01a031329b86ac37cc7f7d718766638f39
   ],
   imports: [
     BrowserModule,
